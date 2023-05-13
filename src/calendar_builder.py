0de6b82b73989f6c187bc5f5e50dce1c1979d2a5
ArtemyBobkov <bobkov.aa@phystech.edu>
2023-05-13 18:28:11 +0300

    ебой

--- src/calendar_builder.py
+++ src/calendar_builder.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 from datetime import datetime, timedelta
 from time import time_ns
@@ -11,10 +12,10 @@ import sqlalchemy.engine
 from numpy import NaN
 from sqlalchemy import text
 
-from definitions import ROOT_DIR, CALENDAR_DIR
-from src.database_filling import connect_sqlalc
+from database_filling import connect_sqlalc
 
-def find_closest_city(city, )
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CALENDAR_DIR = 'calendars'
 
 
 def find_best_date(weather: pd.DataFrame, good_temp: Dict[str, Dict[str, float]], culture: str) -> datetime:
@@ -77,7 +78,8 @@ class CalendarBuilder:
         :param city: Город, для которого генерируется календарь.
         :return: Путь до файла, где лежит календарь в формате json
         """
-        condition = f'weather.\"City\" = \'{city}\''
+        new_city = pd.read_sql_query(select_query('city_x_weather', f'city_x_weather.city = \'{city}\''), con=self.conn)['weather'].item()
+        condition = f'weather.\"City\" = \'{new_city}\''
         weather = pd.read_sql_query(select_query(self.weather_db_name, condition), con=self.conn, index_col='index')
         culture_name = self.cultures_location[self.cultures_location.culture_name == culture].culture_table_name.item()
         culture_df = pd.read_sql_query(select_query(culture_name), con=self.conn)
@@ -107,19 +109,22 @@ class CalendarBuilder:
         for row in features.iterrows():
             feature, min_time, max_time, chore = row[1].tolist()
             if feature in params or feature is None or feature is NaN:
-                chores.append({chore: (int(min_time), int(max_time))})
+                chores.append({"title": chore, "start": int(min_time), "end": int(max_time)})
 
         best_date = find_best_date(weather, usual_temp, culture)
 
         for chore in chores:
-            chore_name = list(chore.keys())[0]
-            min_date, max_date = chore[chore_name]
-            chore[chore_name] = (best_date + timedelta(min_date), best_date + timedelta(max_date))
+            chore_name = chore["title"]
+            min_date = chore["start"]
+            max_date = chore["end"]
+
+            chore["start"] = best_date + timedelta(min_date)
+            chore["end"] = best_date + timedelta(max_date)
 
         return chores
 
-
 if __name__ == '__main__':
+    culture, sort, city = sys.argv[1], sys.argv[2], sys.argv[3]
     engine = connect_sqlalc()
     builder = CalendarBuilder(
         'culture_x_temperature',
@@ -128,5 +133,8 @@ if __name__ == '__main__':
         'features_technical',
         engine.connect()
     )
-    print(builder.create_calendar('Томат', 'Томат Ранний красный', 'Брест'))
-    print(builder.create_calendar('Томат', 'Томат Брутус', 'Москва'))
+    # print(builder.create_calendar(culture, sort, city))
+    # print(builder.create_calendar('Томат', 'Томат Ранний красный', 'Брест'))
+    # print(builder.create_calendar('Томат', 'Томат Брутус', 'Белые Столбы'))
+    # 'Томат' 'Томат Ранний красный' 'Брест'
+    # for debug
